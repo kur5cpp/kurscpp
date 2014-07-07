332b9c80994b40cc0a44a5984848c097e13df995
Marcin <kur5cpp@users.noreply.github.com>
2014-07-07 11:58:44 +0200

    test z mackiem

--- programiki/silnia/main.cpp
+++ programiki/silnia/main.cpp
@@ -40,7 +40,31 @@
 //    return 0;
 //}
 
-//wielka suma extended
+////wielka suma extended
+//
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    int a=1, suma=0;
+//    char tn='T';
+//    while(tn=='T')
+//    {
+//        cout << "Podawaj liczby do zsumowania:" << endl;
+//        cin >> a;
+//        suma+=a;
+//        cout<<"dotychczasowa suma:\n";
+//        cout<<suma<<endl;
+//        cout << "Czy chcesz kontynuowac? (T/N)"<<endl;
+//        cin>>tn;
+//    };
+//    cout<<"Dziekujemy za skorzystanie z naszych uslug!!";
+//    return 0;
+//}
+
+//wielka suma ultimate
 
 #include <iostream>
 
@@ -48,18 +72,26 @@ using namespace std;
 
 int main()
 {
-    int a=1, suma=0;
+    int a=1, suma=0, piatki=0, maximum=0,licznik=0;
+    float srednia;
     char tn='T';
     while(tn=='T')
     {
         cout << "Podawaj liczby do zsumowania:" << endl;
         cin >> a;
         suma+=a;
+        licznik++;
+        if(a==5) piatki++;
+        if(a>maximum) maximum=a;
         cout<<"dotychczasowa suma:\n";
         cout<<suma<<endl;
         cout << "Czy chcesz kontynuowac? (T/N)"<<endl;
         cin>>tn;
     };
+    srednia=(float)suma/licznik;
+    cout<<"Srednia wynosi: "<<srednia<<endl;
+    cout<<"Maximum: "<<maximum<<endl;
+    cout<<"Piateczki: "<<piatki<<endl;
     cout<<"Dziekujemy za skorzystanie z naszych uslug!!";
     return 0;
 }
