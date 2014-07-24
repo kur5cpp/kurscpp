d5914876a947b00539f8727ed4adf8897663c770
Marcin <kur5cpp@users.noreply.github.com>
2014-07-24 12:43:09 +0200

    27_7: innout

---
+++ programiki/24_7_/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+int main()
+{
+    ofstream plik;
+    plik.open("dane1.txt");
+    for (int i = 0; i<9; i++)
+        plik <<i<<endl;
+    plik.close();
+//
+    plik.open("dane.txt");
+    int a=5, b=4, c=3;
+    plik<<a<<"\n"<<b<<endl<<c<<endl;
+    plik<<endl<<++a;
+    plik.close();
+
+    cout << "End is a matter of time" << endl;
+    return 0;
+}
