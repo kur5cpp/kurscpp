e3b63cf99e3fd017375edf16aaf4ea5c2116554c
Marcin_Nokia <kur5cpp@users.noreply.github.com>
2014-09-16 11:38:00 +0200

    smart_ptr with template

--- programiki/smart_ptr/main.cpp
+++ programiki/smart_ptr/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 
-class IntPtr //RAII auto_ptr
+class IntPtr
 {
 public:
-    IntPtr();
     IntPtr(int *x)
     : _x(x)
     {}
@@ -16,34 +15,160 @@ public:
 
     ~IntPtr()
     {
+        if( _x)
+        {
+            std::cout << "D'tor called\n";
+        }
         delete _x;
+
     }
 
-    void operator=(const IntPtr &other)
+    void operator=(IntPtr &other)
     {
         moveToMe(other);
     }
-    int *_x;
+
 protected:
     void moveToMe(const IntPtr &other)
     {
         _x = other._x;
-        *other._x = 0;
+        other._x = 0;
     }
+
 private:
-    //int *_x;
+    mutable int *_x;
 };
 
-using namespace std;
+template <class Type>
+class IntSharedPtr
+{
+public:
+    IntSharedPtr()
+    : _referenceCounter(0)
+    {
+    }
+
+    IntSharedPtr(Type *x)
+    : _referenceCounter(new ReferenceCounter(x))
+    {
+    }
+
+    IntSharedPtr(const IntSharedPtr &other)
+    : _referenceCounter(other._referenceCounter)
+    {
+        _referenceCounter->increase();
+    }
+
+    ~IntSharedPtr()
+    {
+        _referenceCounter->decrease();
+        releaseIfNeeded();
+   }
+
+    Type* get()
+    {
+        return _referenceCounter->get();
+    }
+
+    /// @todo For debugging purposes
+    Type counter()
+    {
+        return _referenceCounter->counter();
+    }
+
+    void operator=(IntSharedPtr &other)
+    {
+        if( _referenceCounter != 0 )
+        {
+            _referenceCounter->decrease();
+            releaseIfNeeded();
+        }
+        _referenceCounter = other._referenceCounter;
+        _referenceCounter->increase();
+    }
+protected:
+    void releaseIfNeeded()
+   {
+        if(_referenceCounter != 0 && _referenceCounter->counter() == 0)
+        {
+            std::cout << "D'tor called\n";
+            delete _referenceCounter;
+            _referenceCounter = 0;
+        }
+    }
+
+private:
+    struct ReferenceCounter
+    {
+        ReferenceCounter(Type *x)
+            : _x(x)
+            , _counter(1)
+        {}
+
+        ~ReferenceCounter()
+        {
+            delete _x;
+        }
+
+        void increase()
+        {
+            ++_counter;
+        }
+
+        void decrease()
+        {
+            --_counter;
+        }
+
+        int counter() const
+        {
+            return _counter;
+        }
+        Type* get()
+        {
+            return _x;
+        }
+
+        int _counter;
+        Type *_x;
+    };
+
+    mutable ReferenceCounter *_referenceCounter;
+};
 
 int main()
 {
-    IntPtr A(new int(10));
+//    {
+//        IntPtr ptr(new int(9));
+//        IntPtr ptr2 = ptr;
+//    }
+    {
+        IntSharedPtr <int> ptr1(new int(8));
+        std::cout << "counter=" << ptr1.counter() << "\n";
+       {
+            IntSharedPtr <int> ptr2 = ptr1;
+            std::cout << "Ptr=" << (ptr1.get() == ptr2.get()) << "\n";
+            std::cout << "counter=" << ptr1.counter() << "\n";
+            std::cout << "counter=" << ptr2.counter() << "\n";
 
-    *A._x=10;
-    cout<<*A._x<<endl;
-    IntPtr B=A;
-    cout<<*B._x<<endl;
-    cout << "Hello world!" << endl;
+        }
+
+        std::cout << "counter=" << ptr1.counter() << "\n";
+    }
+    std::cout << "----------------------------\n";
+    {
+        IntSharedPtr <int> ptr1(new int(1));
+        std::cout << "counter=" << ptr1.counter() << "\n";
+        {
+            IntSharedPtr <int> ptr2 (new int(2));
+            ptr2 = ptr1;
+            std::cout << "Ptr=" << (ptr1.get() == ptr2.get()) << "\n";
+            std::cout << "counter=" << ptr1.counter() << "\n";
+            std::cout << "counter=" << ptr2.counter() << "\n";
+
+        }
+
+       std::cout << "counter=" << ptr1.counter() << "\n";
+    }
     return 0;
 }
